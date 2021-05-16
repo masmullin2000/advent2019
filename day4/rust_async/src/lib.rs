26b5af3e64e69c3ca34471dbf4516fe0851e3913
Michael Mullin <masmullin@gmail.com>
2021-05-16 01:01:42 -0400

    performance increase from https://gist.github.com/mike-barber/12fa764e1f5067ea5d352a51815b8d0c

--- day4/rust_async/src/lib.rs
+++ day4/rust_async/src/lib.rs
@@ -1,29 +1,37 @@
+/// Performance modifications using normal Rust Strings instead of needing to resort to arrays of u8
+///
+/// Notes
+/// - still uses `itoa::fmt` to format a string to an existing String buffer quickly -- it's faster
+//    than calling `write!(...)`
+/// - reuses a `String`, clearing it rather than re-allocating
+/// - uses `chars()` iterator to do a single pass along the string
+/// - using generic `<F>` parameter rather than function pointers, to permit monomorphization
+///
+/// On my machine, it's slightly faster than the [u8] algorithm too. Might be bounds checking.
+///
+/// Original: https://github.com/masmullin2000/advent2019/blob/main/day4/rust_async/src/lib.rs
+///
+/// And credits to commenters Dylan Foxx et al on the video https://www.youtube.com/watch?v=E-47VLwMY_U for comments 
+/// originally pointing out how to use the chars() iterator.
+
+
 use anyhow::Result;
-use std::thread;
-
-extern crate itoa;
-
-use itoa::*;
-
 use tokio::spawn;
 
-type FuncVal = fn(&[u8]) -> bool;
-
-fn validate(password: &[u8]) -> bool {
-    let mut rc = false;
-    if password.len() < 6 {
+fn validate(password: &str) -> bool {
+    // we want exactly 6 bytes
+    // each char should be only one byte
+    if password.len() != 6 {
         return false;
     }
 
-    // when using string
-    // let mut prev = password.chars().nth(0).unwrap();
-    // for i in 1..6 {
-    // 	let test = password.chars().nth(i).unwrap();
+    let mut rc = false;
+    
+    // using iterator, take first character, then compare remaining ones
+    let mut chars_iter = password.chars();
+    let mut prev = chars_iter.next().unwrap();
 
-    let mut prev = password[0];
-    for i in 1..6 {
-        let test = password[i];
-
+    for test in chars_iter.take(5) {
         if test < prev {
             return false;
         }
@@ -38,20 +46,20 @@ fn validate(password: &[u8]) -> bool {
     rc
 }
 
-fn validate_extra(password: &[u8]) -> bool {
-    let mut rc = false;
-    if password.len() < 6 {
+fn validate_extra(password: &str) -> bool {
+    // we want exactly 6 bytes
+    // each char should be only one byte
+    if password.len() != 6 {
         return false;
     }
+    let mut rc = false;
 
-    // let mut m = '0';
-    // let mut prev = password.chars().nth(0).unwrap();
-    // for i in 1..6 {
-    //    	let test = password.chars().nth(i).unwrap();
-    let mut m = '0' as u8;
-    let mut prev = password[0];
-    for i in 1..6 {
-        let test = password[i];
+    // using iterator, take first character, then compare remaining ones
+    let mut chars_iter = password.chars();
+    let mut prev = chars_iter.next().unwrap();
+
+    let mut m = '0';
+    for test in chars_iter.take(5) {
         if test < prev {
             return false;
         }
@@ -59,26 +67,31 @@ fn validate_extra(password: &[u8]) -> bool {
         if test == prev {
             if test == m {
                 rc = false;
-            } else if m == '0' as u8 || rc == false {
+            } else if m == '0' || rc == false {
                 rc = true;
                 m = test;
             }
         }
+
         prev = test;
     }
-
     rc
 }
 
-fn count_valid(start: i64, end: i64, val_func: FuncVal) -> i64 {
+fn count_valid<F: Fn(&str) -> bool>(start: i64, end: i64, val_func: F) -> i64 {
     let mut count = 0;
 
+    // re-usable string, to avoid allocating repeatedly
+    let mut password = String::new();
     for i in start..=end {
-        //let password = i.to_string();
+        // clear and append the password
+        password.clear();
+        itoa::fmt(&mut password, i).unwrap();
 
-        let mut bytes = [0u8; 6];
-        itoa::write(&mut bytes[..], i);
-        if val_func(&bytes) {
+        // slower way, via the normal formatter
+        //write!(password, "{}", i).unwrap();
+
+        if val_func(&password) {
             count += 1;
         }
     }
@@ -87,7 +100,10 @@ fn count_valid(start: i64, end: i64, val_func: FuncVal) -> i64 {
 }
 
 #[tokio::main]
-async fn dowork(mut min: i64, max: i64, amt: i64, parallel: i64, val_func: FuncVal) -> i64 {
+async fn dowork<F>(mut min: i64, max: i64, amt: i64, parallel: i64, val_func: F) -> i64
+where
+    F: 'static + Fn(&str) -> bool + Send + Copy
+{
     let mut tasks = vec![];
 
     for _ in 0..parallel {
@@ -142,37 +158,37 @@ mod tests {
 
     #[test]
     fn test_valid() {
-        assert_eq!(true, validate("111111".as_bytes()));
-        assert_eq!(false, validate("223450".as_bytes()));
-        assert_eq!(false, validate("123789".as_bytes()));
+        assert_eq!(true, validate("111111"));
+        assert_eq!(false, validate("223450"));
+        assert_eq!(false, validate("123789"));
     }
 
     // #[test]
     // fn test_count_valid() {
-    // 	assert_eq!(1, count_valid(111111,111111, validate));
-    // 	assert_eq!(2, count_valid(111111,111112, validate));
-    // 	assert_eq!(0, count_valid(1,99999, validate));
-    // 	assert_eq!(979, count_valid(256310, 732736, validate));
+    //  assert_eq!(1, count_valid(111111,111111, validate));
+    //  assert_eq!(2, count_valid(111111,111112, validate));
+    //  assert_eq!(0, count_valid(1,99999, validate));
+    //  assert_eq!(979, count_valid(256310, 732736, validate));
     // }
 
     // #[test]
     // fn test_valid_extra() {
-    // 	assert_eq!(false, validate_extra("111111"));
-    // 	assert_eq!(false, validate_extra("223450"));
-    // 	assert_eq!(false, validate_extra("123789"));
+    //  assert_eq!(false, validate_extra("111111"));
+    //  assert_eq!(false, validate_extra("223450"));
+    //  assert_eq!(false, validate_extra("123789"));
 
-    // 	assert_eq!(true, validate_extra("112233"));
-    // 	assert_eq!(false, validate_extra("123444"));
-    // 	assert_eq!(true, validate_extra("111122"));
-    // 	assert_eq!(true, validate_extra("112222"));
-    // 	assert_eq!(false, validate_extra("111222"));
+    //  assert_eq!(true, validate_extra("112233"));
+    //  assert_eq!(false, validate_extra("123444"));
+    //  assert_eq!(true, validate_extra("111122"));
+    //  assert_eq!(true, validate_extra("112222"));
+    //  assert_eq!(false, validate_extra("111222"));
     // }
 
     // #[test]
     // fn test_count_valid_extra() {
-    // 	assert_eq!(0, count_valid(111111,111111, validate_extra));
-    // 	assert_eq!(0, count_valid(111111,111112, validate_extra));
-    // 	assert_eq!(0, count_valid(1,99999, validate_extra));
-    // 	assert_eq!(635, count_valid(256310, 732736, validate_extra));
+    //  assert_eq!(0, count_valid(111111,111111, validate_extra));
+    //  assert_eq!(0, count_valid(111111,111112, validate_extra));
+    //  assert_eq!(0, count_valid(1,99999, validate_extra));
+    //  assert_eq!(635, count_valid(256310, 732736, validate_extra));
     // }
 }
